9d2abfd6ab6ac75156b1767cfa4d246646e2586a
Rahul Sharma <rahulsrma26@gmail.com>
2019-03-14 03:21:42 +0530

    Added fetcher factory for future uses (multiple sites)

---
+++ commands/fetchers/code_force.py
@@ -0,0 +1,23 @@
+from urllib.parse import urlsplit
+from bs4 import BeautifulSoup
+
+from .base_fetcher import BaseFetcher
+
+
+class CodeForce(BaseFetcher):
+
+    def dirname(self):
+        comps = urlsplit(self.url).path.split('/')
+        return 'CF{}-D2-{}'.format(comps[2], comps[4])
+
+    def tests(self):
+        soup = BeautifulSoup(self.text, 'html.parser')
+
+        previous = None
+        for div in soup.find('div', {'class': 'sample-test'}):
+            dtype = div['class'][0]
+            text = div.find('pre').get_text('\n')
+            if dtype == 'input':
+                previous = text
+            elif dtype == 'output':
+                yield previous, text
