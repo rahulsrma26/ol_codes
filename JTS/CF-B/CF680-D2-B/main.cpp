8b862b4ef8af236756bdd28c1c2e318f12d5a9b7
Rahul Sharma <rahulsrma26@gmail.com>
2019-03-20 22:22:32 +0530

    JTS CF680 B added

---
+++ JTS/CF-B/CF680-D2-B/main.cpp
@@ -0,0 +1,27 @@
+//    Date : 2019-03-20
+//  Author : Rahul Sharma
+// Problem : http://codeforces.com/contest/680/problem/B
+
+#include <bits/stdc++.h>
+
+int main() {
+    using namespace std;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    int c = 0;
+    for (auto& x : a) {
+        cin >> x;
+        c += x;
+    }
+
+    for (int l = k - 2, r = k; 0 <= l && r < n; l--, r++)
+        if(a[l] + a[r] == 1)
+            c--;
+    cout << c << '\n';
+}
